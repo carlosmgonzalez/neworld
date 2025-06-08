d59e91539a2a1558f064709ad2980c6f25201b30
carlosmgonzalez <carlosmgonzalez1998@gmail.com>
2025-06-08 13:09:39 -0300

    mejora en la imagen y alinear precio con stock

--- src/components/ui/ProductCard.svelte
+++ src/components/ui/ProductCard.svelte
@@ -10,7 +10,38 @@
 	href={`/product/${product.id}`}
 	class="bg-blue-200/50 shadow-lg rounded-xl flex flex-col items-center justify-start gap-1 hover:opacity-80 transition-opacity"
 >
-	<img
+	<div
+		class="w-full aspect-square overflow-hidden rounded-t-xl
+	bg-gray-100"
+	>
+		<img
+			src={product.images[0]}
+			alt={product.name}
+			class="w-full h-full object-cover object-center
+		transition-transform hover:scale-105"
+		/>
+	</div>
+
+	<div class="flex flex-col px-2 pb-2 w-full">
+		<h3 class="font-medium text-xs md:text-sm">
+			{product.name}
+		</h3>
+		<p class="text-xxs md:text-xs text-neutral-600">
+			{product.smallDescription}
+		</p>
+		<div class="flex justify-between items-center mt-2">
+			{#if product.stock > 0}
+				<p class="text-xs text-emerald-600">En stock</p>
+			{:else}
+				<p class="text-xs text-red-600">Sin stock</p>
+			{/if}
+			<p class="text-xs md:text-sm font-semibold">
+				{formatPrice(product.price)}
+			</p>
+		</div>
+	</div>
+
+	<!-- <img
 		src={product.images[0]}
 		alt={product.name}
 		class="w-full h-auto rounded-t-xl aspect-square object-contain"
@@ -18,13 +49,13 @@
 	<div class="flex flex-col px-2 pb-2 w-full">
 		<h3 class="font-medium text-xs md:text-sm">{product.name}</h3>
 		<p class="text-xxs md:text-xs text-neutral-600 self-start">{product.smallDescription}</p>
-		<div class="flex flex-row justify-between items-center">
+		<div class="flex flex-row justify-between items-center mt-2">
 			{#if product.stock > 0}
 				<p class="text-xs text-emerald-600">En stock</p>
 			{:else}
 				<p class="text-xs text-red-600">Sin stock</p>
 			{/if}
-			<p class="text-xs md:text-sm font-semibold mt-2">{formatPrice(product.price)}</p>
+			<p class="text-xs md:text-sm font-semibold">{formatPrice(product.price)}</p>
 		</div>
-	</div>
+	</div> -->
 </a>
